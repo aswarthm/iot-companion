461711bf48339bfd714590d2bb8cc5657acbb5cf
aswarth <m.aswarthreddy@gmail.com>
2022-11-21 12:56:00 +0530

    save cart in localStorage

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./Layout";
 import Home from "./Home";
 import Cart from "./Cart";
+import Track from "./Track";
 
 function App() {
   const [products, setProducts] = React.useState([]);
-
+  
   React.useEffect(() => {
     fetch("https://iot-companion.azurewebsites.net/api/getItems?")
     .then(response => response.json())
     .then(data => setProducts(data))
   }, [])
 
-  const [cart, setCart] = React.useState([]);
+  const [cart, setCart] = React.useState(JSON.parse(window.localStorage.getItem("cartItems")) || []);
+
+  React.useEffect(() => {
+    setCart(JSON.parse(window.localStorage.getItem("cartItems")) || [])
+  }, [])
+
+  React.useEffect(() => {
+    window.localStorage.setItem("cartItems", JSON.stringify(cart))
+  }, [cart])
 
   return (
     <BrowserRouter>
@@ -22,6 +31,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home products={products} setCart={setCart} cart={cart} />} />
           <Route path="cart" element={<Cart setCart={setCart} cart={cart} />} />
+          <Route path="track" element={<Track />} />
         </Route>
       </Routes>
     </BrowserRouter>
