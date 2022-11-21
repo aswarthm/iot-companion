85b7926f6ea11a9fec5bf95d52da741d335ba3f0
aswarth <m.aswarthreddy@gmail.com>
2022-11-21 12:53:16 +0530

    add cart checkout logic

--- src/Cart.js
+++ src/Cart.js
@@ -6,27 +6,37 @@ function Cart({ cart, setCart }) {
   function getCartItems({ cart }) {
     if(cart.length === 0){
         return(
-            <div>
-                Cart is Empty
+            <div className="cart empty">
+                Wow, such empty :0
             </div>
         )
     }
     else{
         return(
-            cart.map((item) => (
-              <div key={item.product_code} className="cartItem">
-                <div>
-                  <div>{item.name}</div>
-                  <div className="cartItemPrice">{item.price}</div>
+          <>
+            <div className="cart">
+              {cart.map((item) => (
+                <div key={item.product_code} className="cartItem">
+                  <div>
+                    <div>{item.name}</div>
+                    <div className="cartItemPrice">{item.price}</div>
+                  </div>
+                  <div className="cartItemQuantityHandler">
+                    <ItemQuantityHandler item={item} setCart={setCart} />
+                    <div className="itemTotalPrice">{getItemTotal({item})}</div>
+                  </div>
                 </div>
-                <div className="cartItemQuantityHandler">
-                  <ItemQuantityHandler item={item} setCart={setCart} />
-                  <div className="itemTotalPrice">{getItemTotal({item})}</div>
-                </div>
+              ))}
+            </div>
+            <div className="checkout">
+              <div className="cartTotalPrice">Cart Total: {getCartTotal()}</div>
+              <div className="checkoutBtn" role="button" onClick={() => handleCheckout()}>Checkout</div>
+            </div>
               </div>
             ))
+          </>
         )
-    }
+      }
   }
 
   function getItemTotal({item}){
@@ -43,16 +53,38 @@ function Cart({ cart, setCart }) {
     return(cartTotal)
   }
 
+  async function handleCheckout(){
+
+    setLoading(true)
+
+    const order = {
+      order_id: "111",  
+      cust_name: "lol",
+      items: cart
+    }
+    
+    const orderReq = await fetch("https://iot-companion.azurewebsites.net/api/newOrder?code=gWR-FEj-10XwvCgTEKponrY2wtPLxIgTMqJ-f_O1xK1VAzFu4eg9FA==",
+                    {  
+                      headers: {
+                        'accept': 'application/json',
+                        'Content-Type': 'application/json'
+                      },
+                      'method': 'POST',
+                      'body': JSON.stringify(cart)
+                    })
+
+    const orderResp = await orderReq.json()
+    console.log(orderResp)
+
+    //setCart([])
+    setLoading(false)
+  }
+
   return (
     <>
       <h1>Cart</h1>
-      <div className="cart">
-          {getCartItems({cart})}
-      </div>
-      <div className="checkout">
-        <div className="cartTotalPrice">Cart Total: {getCartTotal()}</div>
-        <div className="checkoutBtn" role="button">Checkout</div>
-      </div>
+      {getCartItems({cart})}
+
     </>
   );
 }
